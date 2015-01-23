9f9ab70ab222fbebb7623401b500fb559677c85c
Steven Ng <steven.ng@temple.edu>
2015-01-23 15:26:52 -0500

    Show thumbnails and collection link in scaffold

--- spec/views/digital_collections/index.html.erb_spec.rb
+++ spec/views/digital_collections/index.html.erb_spec.rb
@@ -6,24 +6,29 @@ RSpec.describe "digital_collections/index", :type => :view do
       DigitalCollection.create!(
         :collection_alias => "Alias1",
         :name => "Name",
-        :thumbnail_url => "Thumbnail Url",
+        :thumbnail_url => "thumbnail_url.png",
         :description => "MyText"
       ),
       DigitalCollection.create!(
         :collection_alias => "Alias2",
         :name => "Name",
-        :thumbnail_url => "Thumbnail Url",
+        :thumbnail_url => "thumbnail_url.png",
         :description => "MyText"
       )
     ])
   end
+
+  let (:collection_1_url) { path(DigitalCollection.first) }
+  let (:collection_2_url) { path(DigitalCollection.last) }
 
   it "renders a list of digital_collections" do
     render
     assert_select "tr>td", :text => "Alias1".to_s, :count => 1
     assert_select "tr>td", :text => "Alias2".to_s, :count => 1
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Thumbnail Url".to_s, :count => 2
+    assert_select "tr>td", :text => "thumbnail_url.png".to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "a[href='#{collection_1_url}']", :count => 2
+    assert_select "a[href='#{collection_2_url}']", :count => 2
   end
 end
