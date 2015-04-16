46d4d0024ab51b11fcecca30366fa435bc077fc0
Katherine Lynch <katherine.lynch@temple.edu>
2015-04-16 16:49:09 -0400

    Added link to collection, more styling

--- app/helpers/tul_cdm_helper.rb
+++ app/helpers/tul_cdm_helper.rb
@@ -477,7 +477,7 @@ module TulCdmHelper
     images = ''
     DigitalCollection.find_each do |dig_col|
       if dig_col.featured.eql?("Yes")
-        coll_name = content_tag(:div, dig_col.name, :class => "featured-collection-name")
+        coll_name = content_tag(:div, link_to(dig_col.name, "/digital_collections/#{dig_col.id}"), :class => "featured-collection-name")
         coll_descrip = content_tag(:div, truncate(dig_col.description, length: 300, omission: '...', escape: false, separator: " "), :class => "featured-collection-descrip")
         slide_content = content_tag(:div, coll_name + coll_descrip.html_safe, :class => "featured-collection-text-container") + image_tag(dig_col.image_url, :alt => dig_col.name) 
         images += content_tag(:div, slide_content, :class => "featured-collection-image")
