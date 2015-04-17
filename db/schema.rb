a80664179e3dbecc44e1753179f72b58bb97f2df
Katherine Lynch <katherine.lynch@temple.edu>
2015-04-17 12:52:04 -0400

    Spruced up collections index page a bit, added accessible alt text to browse thumbnails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414174252) do
+ActiveRecord::Schema.define(version: 20150415193428) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150414174252) do
     t.datetime "updated_at"
     t.string   "image_url"
     t.integer  "priority"
+    t.string   "featured"
     t.boolean  "is_private"
     t.string   "allowed_ip_addresses"
   end
