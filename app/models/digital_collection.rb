ff74882bbd7a1644be29d17add823fbf82351a48
Katherine Lynch <katherine.lynch@temple.edu>
2015-04-21 16:14:00 -0400

    Implemented friendly IDs for collection alias in URL, updated homepage text per stakeholder's request

--- app/models/digital_collection.rb
+++ app/models/digital_collection.rb
@@ -1,3 +1,5 @@
 class DigitalCollection < ActiveRecord::Base
   validates :collection_alias, presence: true, uniqueness: true
+  extend FriendlyId
+  friendly_id :collection_alias, use: [:slugged, :finders]
 end
