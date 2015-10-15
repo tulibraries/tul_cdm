343d7e147209e39c4920d9865323f273b7468ba9
Steven Ng <steven.ng@temple.edu>
2015-10-15 15:51:13 -0400

    Handle add and remove facet selection
    - JQuery both adds and removes facet selections from search form
    - Must explicitly add multiselect_facets script

--- app/assets/javascripts/multiselect_facets.js
+++ app/assets/javascripts/multiselect_facets.js
@@ -4,16 +4,19 @@ $(document).ready(function() {
   });
 });
 
+// Handles multifacet by adding the hidden search term field field to search form
 function click_multifacet(event){
-    var facet_item = event.target.value
-    var facet_name = event.target.name
-    var checked = event.target.checked
-    var selector = 'form.search-query-form > input[type="hidden"][name="' + facet_name + '"][value="' + facet_item + '"]'
+  var facet_item_value = event.target.value;
+  var facet_item_name = event.target.name;
+  var facet_item_selector = 'form.search-query-form > input[type="hidden"][name="' + facet_item_name + '"][value="' + facet_item_value + '"]';
+  var facet_item_element = '<input name="' + facet_item_name + '" type="hidden" value="' + facet_item_value + '">'
+  var facet_item_insertion_selector = 'form.search-query-form > div.input-group';
 
-    if (!checked) {
-      console.log(facet_item + " unchecked");
-      $(selector).remove;
-    }
-
+  if (event.target.checked) {
+    if ($(facet_item_selector).length == 0) 
+      $(facet_item_insertion_selector).before(facet_item_element);
+  }
+  else {
+    $(facet_item_selector).remove();
+  }
 }
-
