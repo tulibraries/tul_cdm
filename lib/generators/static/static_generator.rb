c223e842c17b6d7b867b4c3c4b2f34c9ad518cd7
Steven Ng <steven.ng@temple.edu>
2016-01-07 15:14:29 -0500

    Insert route to static page in routes file

--- lib/generators/static/static_generator.rb
+++ lib/generators/static/static_generator.rb
@@ -4,14 +4,29 @@ class StaticGenerator < Rails::Generators::Base
   argument :path, type: :string, default: 'app/views/pages'
 
   def generate_static
-    template "static_page.html", filename
+    @file_name = page_name.parameterize('_')
+    @file_path = File.join(path, @file_name + '.html')
+
+    #template "static_page.html", @file_path
+    update_route
   end
 
   private
 
-  def filename
-    filename = page_name.parameterize('_') + '.html'
-    filepath = path
-    File.join(filepath, filename)
+  def update_route
+    new_route = sprintf("get '%s' => 'high_voltage/pages#show', id: '%s'", @file_name, @file_name)
+
+    # Extract the contents of the routes file
+    r = ""
+    File.open("config/routes.rb") do |routes_file|
+      while (line = routes_file.gets) do
+        r << line
+      end
+    end
+
+    # Insert in the new route entry before the last end statement
+    File.open("config/routes.rb", "w") do |routes_file|
+      routes_file.write(r.gsub(/^(\s+)end(\.*)$/,"\n  #{new_route}\\1end\\2"))
+    end
   end
 end
