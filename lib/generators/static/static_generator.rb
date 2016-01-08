06d7e90724cd1af661a21cdea4c3dff0bdd85962
Steven Ng <steven.ng@temple.edu>
2016-01-08 08:54:15 -0500

    Make path optional and clean up regular expression
    - Make path an option
    - Fixes bugs in regular expression
    - Add git branch option

--- lib/generators/static/static_generator.rb
+++ lib/generators/static/static_generator.rb
@@ -1,14 +1,16 @@
 class StaticGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   argument :page_name, type: :string
-  argument :path, type: :string, default: 'app/views/pages'
+  class_option :path, type: :string, default: 'app/views/pages', description: 'Path to save page, Default: app/views/pages'
+  class_option :branch, type: :string, default: '', description: 'Git branch name. Creates branch if branch_identifier specified'
 
   def generate_static
     @file_name = page_name.parameterize('_')
-    @file_path = File.join(path, @file_name + '.html')
+    @file_path = File.join(options.path, @file_name + '.html')
 
     template "static_page.html", @file_path
     update_route
+    create_branch unless options.branch.blank?
   end
 
   private
@@ -26,7 +28,15 @@ class StaticGenerator < Rails::Generators::Base
 
     # Insert in the new route entry before the last end statement
     File.open("config/routes.rb", "w") do |routes_file|
-      routes_file.write(r.gsub(/^(\s+)end(\.*)$/,"\n  #{new_route}\\1end\\2"))
+      routes_file.write(r.gsub(/(\n\s*end\s*)$/m,"\n  #{new_route}\\1"))
     end
+  end
+
+  def create_branch
+    branch_name = "#{options.branch}-#{page_name.gsub(/ /, '_')}"
+    puts "$ git flow feature start #{branch_name}"
+    puts "$ git commit -a -m 'Create static page \"#{page_name}\"'"
+    puts "$ git flow feature publish #{branch_name}"
+    puts "Edit file at https://github.com/tulibraries/tul_cdm/tree/feature/#{branch_name}/#{@file_path}"
   end
 end
