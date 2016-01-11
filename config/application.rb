bcd6cb575560dbe323dcf3dff82e0c21d1a5da18
Steven Ng <steven.ng@temple.edu>
2016-01-11 14:19:36 -0500

    Implement caching

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module TulCdm
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.cache_store = :file_store, %W(#{config.root}/tmp/cache/ocr)
   end
 end
