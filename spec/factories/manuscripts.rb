04972344ce35c4b83eed1abf0d431610481c665d
Steven Ng <steven.ng@temple.edu>
2016-03-16 10:40:08 -0400

    Show proper numbering for all results pages
    - TODO: Fix spec tests

--- spec/factories/manuscripts.rb
+++ spec/factories/manuscripts.rb
@@ -3,7 +3,16 @@
 FactoryGirl.define do
   factory :manuscript do
     # objectMetadata attributes
-    title ["Manuscript Title"]
+    title ["Manuscript Title #{n}"]
+    contentdm_collection_id "p16002coll14"
+    type ["minutes", "correspondence", "reports", "notes"]
+    format ["image/jp2"]
+  end
+
+  factory :manuscript_list , class: Manuscript do
+    # objectMetadata attributes
+    sequence(:title) {|n| ["Manuscript Title #{n}"] }
+    contentdm_collection_id "p16002coll14"
     type ["minutes", "correspondence", "reports", "notes"]
     format ["image/jp2"]
   end
