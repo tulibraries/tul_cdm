b13a2791fda8d35038d81e2317691e4543dba9e0
Steven Ng <steven.ng@temple.edu>
2016-03-16 17:04:54 -0400

    Test CRD capability
    - Provides example of how data can be formatted

--- spec/lib/cdm_harvester_spec.rb
+++ spec/lib/cdm_harvester_spec.rb
@@ -4,6 +4,9 @@ require 'cdm_harvester'
 RSpec.describe HarvestedCollection do
 
   let (:collection) { FactoryGirl.build(:harvested_collection) }
+  let (:first_pid) { collection.xml_objects.keys.first }
+  let (:last_pid) { collection.xml_objects.keys.last }
+
   subject (:h) {
     h = HarvestedCollection.new
     h.digital_collection_id = collection.digital_collection_id
@@ -18,11 +21,31 @@ RSpec.describe HarvestedCollection do
 
   end
 
-  describe "Delete Harvested Collection Item" do
-    it "deletes the xml objects"
+  describe "Shows Harvested Collection items" do
+
+    it "shows first objects" do
+      expect(subject.xml_objects[first_pid]).to eq(collection.xml_objects[first_pid])
+    end
+
+    it "shows last objects" do
+      expect(subject.xml_objects[last_pid]).to eq(collection.xml_objects[last_pid])
+    end
+
   end
 
-  describe "Delete Harvested Collection" do
-    it "deletes the xml objects"
+  describe "Delete Harvested Collection Item" do
+
+    it "deletes an xml objects" do
+      expect(subject.xml_objects[first_pid]).to be
+      subject.xml_objects.delete(first_pid)
+      expect(subject.xml_objects[first_pid]).to_not be
+    end
+
+    it "deletes another xml objects" do
+      expect(subject.xml_objects[last_pid]).to be
+      subject.xml_objects.delete(last_pid)
+      expect(subject.xml_objects[last_pid]).to_not be
+    end
+
   end
 end
