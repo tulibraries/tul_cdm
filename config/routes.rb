ea49a745d022dae7f35306f9e59d889d90ce1b89
Steven Ng <steven.ng@temple.edu>
2016-04-13 16:44:35 -0400

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,11 @@ Rails.application.routes.draw do
   get '/digital_collections/stereotypicalimages/about' => 'high_voltage/pages#show', id: 'stereotypicalabout'
   get 'catalog/multiselect_facet/:id', to: 'catalog#multiselect_facet'
   get '/cdm/restricted', to: 'digital_collections#restricted'
+  get 'pages/ml_blitzstein_and_co' => 'high_voltage/pages#show', id: 'ml_blitzstein_and_co'
+  get 'pages/m_rosenbaum_and_co' => 'high_voltage/pages#show', id: 'm_rosenbaum_and_co'
+  get 'pages/rosenbluth_brothers' => 'high_voltage/pages#show', id: 'rosenbluth_brothers'
+  get 'pages/pennsylvania_company_for_banking_and_trusts' => 'high_voltage/pages#show', id: 'pennsylvania_company_for_banking_and_trusts'
+  get 'pages/searching_the_ledgers' => 'high_voltage/pages#show', id: 'searching_the_ledgers'
+  get 'pages/philadelphias_steamship_agents' => 'high_voltage/pages#show', id: 'philadelphias_steamship_agents'
 
 end
